a42669d8e270f66eaeb38c84942d1b5d1a2f6fc2
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2015-12-10 18:38:02 +0100

    added initial state probability constructor and getter

--- src/AbstractHMM.hpp
+++ src/AbstractHMM.hpp
@@ -10,13 +10,15 @@ private:
 	S* _states;
 	std::size_t _nStates;
 	float* _transP;
+	float* _initP;
 
 	AbstractHMM(const std::initializer_list<S>& states);
 
 protected:
 	AbstractHMM(
 		const std::initializer_list<S>&, 
-		const std::initializer_list<std::initializer_list<float>>&);
+		const std::initializer_list<std::initializer_list<float>>&,
+		const std::initializer_list<float>&);
 
 	virtual bool state_inBounds(const std::size_t i) const{
 		return (i>0 && i<_nStates);
@@ -25,13 +27,17 @@ protected:
 	virtual bool observation_inBounds(const std::size_t) const = 0;
 
 public:
-	/* Transition probabilities getters*/
+	/* Transition probabilities getters */
 	virtual float trans_p(std::size_t, std::size_t) const;
 	virtual float trans_p_by_object(S, S) const;
 
 	/* Emission probabilities getters, defined in derived classes */
 	virtual float emi_p(std::size_t, std::size_t) const = 0;
 	virtual float emi_p_by_object(S, O) const = 0;
+
+	/* Initial state probabilites getters */
+	virtual float init_p(std::size_t) const;
+	virtual float init_p_by_object(S) const; 
 
 	/* Decoding */
 	virtual void viterbi(const std::array<O>&) = 0;
@@ -41,7 +47,7 @@ public:
 	virtual void train_baumWelch(const std::array<O>&) = 0;
 	virtual void train_stochasticEM(const std::array<O>&) = 0;
 
-	virtual ~AbstractHMM(){ delete[] _states; delete[] _transP; }
+	virtual ~AbstractHMM(){ delete[] _states; delete[] _transP; delete[] _initP; }
 
 	virtual std::size_t find_state(const S state) const{
 		for(std::size_t i=0;i<_nStates;++i){
@@ -73,11 +79,16 @@ AbstractHMM<S, O>::AbstractHMM(const std::initializer_list<S>& states):
 template<typename S, typename O>
 AbstractHMM<S, O>::AbstractHMM(	
 	const std::initializer_list<S>& states,
-	const std::initializer_list<std::initializer_list<float>>& initTransitions):
-		AbstractHMM(states), _transP(new float[states.size()*states.size()]){
-			if(initTransitions.size() != states.size() || initTransitions[0].size() != states.size())
-				throw std::logic_error("Wrong size of init transitions probabilities");
-			std::copy(std::begin(initTransitions), std::end(initTransitions), _transP);
+	const std::initializer_list<std::initializer_list<float>>& transP,
+	const std::initializer_list<float>& initP):
+		AbstractHMM(states), _transP(new float[states.size()*states.size()]), 
+		_initP(new float[states.size()]){
+			if(transP.size() != states.size() || transP[0].size() != states.size())
+				throw std::logic_error("Wrong initializer_list size for transitions probabilities");
+			if(initP.size() != states.size())
+				throw std::logic_error("Wrond initializer_list size for initial probabilities")
+			std::copy(std::begin(transP), std::end(transP), _transP);
+			std::copy(std::begin(initP), std::end(initP), _initP);
 		}
 
 template<typename S, typename O>
@@ -94,5 +105,20 @@ float AbstractHMM<S, O>::trans_p_by_object(S firstState, S secondState) const{
 		throw std::logic_error("State not found");
 	return _transp[i*_nStates + j];
 }
+
+template<typename S, typename O>
+float AbstractHMM<S, O>::init_p(std::size_t i) const{
+	if(!state_inBounds(i))
+		throw std::out_of_range("State out of bounds");
+	return _initP[i]
+}
+
+template<typename S, typename O>
+AbstracHMM<S, O>::init_p_by_object(S state) const{
+	std::size_t i;
+	if(i=find_state(state) == nullptr)
+		throw std::logic_error("State not found");
+	return _initP[i];
+} 
 
 #endif
